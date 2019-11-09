98a1c48eb011057ec52059a5863acf23a86e5ecd
AlejandroRachadell <alejandro.rachadell@gmail.com>
2019-11-09 13:22:04 +0000

    the input fst is now reversed before processing and then is reversed afterwards to provide the result in the original order.

--- run.sh
+++ run.sh
@@ -3,27 +3,33 @@
 
 ################### Transducers ################
 #
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma_noun.txt | fstarcsort > lemma_noun.fst
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma_adv.txt | fstarcsort > lemma_adv.fst
+#fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma_noun.txt | fstarcsort > lemma_noun.fst
+#fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma_adv.txt | fstarcsort > lemma_adv.fst
 
 # Noun
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  noun.txt | fstarcsort > noun.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait noun.fst | dot -Tpdf  > noun.pdf
+#fstcompile --isymbols=syms.txt --osymbols=syms.txt  noun.txt | fstarcsort > noun.fst
+#fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait noun.fst | dot -Tpdf  > noun.pdf
 
 # Adv
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  adv.txt | fstarcsort > adv.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait adv.fst | dot -Tpdf  > adv.pdf
+#fstcompile --isymbols=syms.txt --osymbols=syms.txt  adv.txt | fstarcsort > adv.fst
+#fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait adv.fst | dot -Tpdf  > adv.pdf
 
 # Lemma2Noun
-fstconcat lemma_adv.fst adv.fst > lemma2adv.fst
-fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adv.fst | dot -Tpdf  > lemma2adv.pdf
+#fstconcat lemma_adv.fst adv.fst > lemma2adv.fst
+#fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adv.fst | dot -Tpdf  > lemma2adv.pdf
+
+# Adv
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adv.txt | fstarcsort > lemma2adv.fst
 
 #################################
 # Test run 1
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test1.txt | fstarcsort > test1.fst
-fstcompose test1.fst lemma2noun.fst > testresult1.fst
+# Use the reverse fst of test1
+fstreverse --isymbols=syms.txt --osymbols=syms.txt  test1.fst > test1r.fst
+fstcompose test1r.fst lemma2adv.fst > testresult1r.fst
+# Reverse the result back to normal
+fstreverse --isymbols=syms.txt --osymbols=syms.txt  testresult1r.fst > testresult1.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait testresult1.fst | dot -Tpdf  > testresult1.pdf
-
 
 # Print result
 fstproject --project_output testresult1.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
