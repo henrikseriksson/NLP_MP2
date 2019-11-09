f02bed5de333094118fa529ab47a653572dcdb47
Henrik Eriksson <henrikseriksson@gmail.com>
2019-11-09 18:27:28 +0000

    added lemma, to run through lemma till end

--- run.sh
+++ run.sh
@@ -18,23 +18,31 @@
 #fstconcat lemma_adv.fst adv.fst > lemma2adv.fst
 #fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adv.fst | dot -Tpdf  > lemma2adv.pdf
 
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma.txt | fstarcsort > lemma.fst
+
+
 # Adv
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adv.txt | fstarcsort > lemma2adv.fst
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2adv.txt | fstarcsort > adv.fst
+fstconcat adv.fst lemma.fst > lemma2adv.fst
+
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2adv.fst | dot -Tpdf  > adv.pdf
 
 # Noun
-fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > lemma2noun.fst
+fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2noun.txt | fstarcsort > noun.fst
+fstconcat noun.fst lemma.fst > lemma2noun.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2noun.fst | dot -Tpdf  > adv.pdf
 
 #################################
 # Test run 1
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test1.txt | fstarcsort > test1.fst
 # Use the reverse fst of test1
-fstreverse --isymbols=syms.txt --osymbols=syms.txt  test1.fst > test1r.fst
-fstcompose test1r.fst lemma2adv.fst > testresult1r.fst
+fstreverse test1.fst > test1r.fst
+fstcompose test1r.fst lemma2noun.fst > testresult1r.fst
 #fstcompose test1r.fst lemma2noun.fst > testresult1r.fst
 #later we will use the union between all the lemma2something to test
 
 # Reverse the result back to normal
-fstreverse --isymbols=syms.txt --osymbols=syms.txt  testresult1r.fst > testresult1.fst
+fstreverse testresult1r.fst > testresult1.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait testresult1.fst | dot -Tpdf  > testresult1.pdf
 
 # Print result
