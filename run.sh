fa830df636f034d9f2cc2b39565e7f604603d656
Henrik Eriksson <henrikseriksson@gmail.com>
2019-11-11 22:56:44 +0000

    changed so all first steps can be merged in some way

--- run.sh
+++ run.sh
@@ -46,6 +46,12 @@ fstcompile --isymbols=syms.txt --osymbols=syms.txt  lemma2verbif.txt | fstarcsor
 fstconcat verbif.fst lemma.fst > lemma2verbif.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verbif.fst | dot -Tpdf  > verbif.pdf
 
+# Verb
+fstunion lemma2verbis.fst lemma2verbip.fst > lemma2verb.fst
+fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait lemma2verb.fst | dot -Tpdf  > verb.pdf
+
+
+
 #################################
 # Test run 1
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test1.txt | fstarcsort > test1.fst
@@ -60,46 +66,34 @@ fstproject --project_output testresult1.fst | fstrmepsilon | fsttopsort | fstpri
 
 # Test 2
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test2.txt | fstarcsort > test2.fst
-# Use the reverse fst of test2
 fstreverse test2.fst > test2r.fst
 fstcompose test2r.fst lemma2adverb.fst > testresult2r.fst
-# Reverse the result back to normal
 fstreverse testresult2r.fst > testresult2.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait testresult2.fst | dot -Tpdf  > testresult2.pdf
-# Print result
 fstproject --project_output testresult2.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 
 # Test 3
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test3.txt | fstarcsort > test3.fst
-# Use the reverse fst of test3
 fstreverse test3.fst > test3r.fst
 fstcompose test3r.fst lemma2verbip.fst > testresult3r.fst
-# Reverse the result back to normal
 fstreverse testresult3r.fst > testresult3.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait testresult3.fst | dot -Tpdf  > testresult3.pdf
-# Print result
 fstproject --project_output testresult3.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 
 # Test 4
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test4.txt | fstarcsort > test4.fst
-# Use the reverse fst of test4
 fstreverse test4.fst > test4r.fst
 fstcompose test4r.fst lemma2verbis.fst > testresult4r.fst
-# Reverse the result back to normal
 fstreverse testresult4r.fst > testresult4.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait testresult4.fst | dot -Tpdf  > testresult4.pdf
-# Print result
 fstproject --project_output testresult4.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 
 # Test 5
 fstcompile --isymbols=syms.txt --osymbols=syms.txt  test5.txt | fstarcsort > test5.fst
-# Use the reverse fst of test5
 fstreverse test5.fst > test5r.fst
 fstcompose test5r.fst lemma2verbif.fst > testresult5r.fst
-# Reverse the result back to normal
 fstreverse testresult5r.fst > testresult5.fst
 fstdraw    --isymbols=syms.txt --osymbols=syms.txt --portrait testresult5.fst | dot -Tpdf  > testresult5.pdf
-# Print result
 fstproject --project_output testresult5.fst | fstrmepsilon | fsttopsort | fstprint --acceptor --isymbols=syms.txt
 
 
